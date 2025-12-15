af8ecfb797ea5f91894b6080d38abd9f9fb13b88
minjae-dev <minjae2246@gmail.com>
2025-12-14 17:49:14 -0800

    bottom bar 추가

---
+++ lib/widget/bottom_bar.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class BottomBar extends StatelessWidget {
+  const BottomBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black,
+      child: SizedBox(
+        height: 50,
+        child: TabBar(
+          labelColor: Colors.white,
+          unselectedLabelColor: Colors.white60,
+          tabs: [
+            Tab(
+              icon: Icon(Icons.home, size: 18),
+              child: Text('Home', style: TextStyle(fontSize: 9)),
+            ),
+            Tab(
+              icon: Icon(Icons.search, size: 18),
+              child: Text('Search', style: TextStyle(fontSize: 9)),
+            ),
+            Tab(
+              icon: Icon(Icons.save_alt, size: 18),
+              child: Text('Save', style: TextStyle(fontSize: 9)),
+            ),
+            Tab(
+              icon: Icon(Icons.list, size: 18),
+              child: Text('List', style: TextStyle(fontSize: 9)),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
